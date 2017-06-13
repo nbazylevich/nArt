c2696ed29f630af3072b01bcc77270027c7d9481
Nbazylevich <nbazylevich@gmail.com>
2017-06-13 23:29:49 +0300

    add logo

--- wp-content/themes/ntheme/index.php
+++ wp-content/themes/ntheme/index.php
@@ -1,8 +1,8 @@
 <!---Header-->
 <?php get_header()?>
 
-<div class="main"  style="background-image: url('<?php bloginfo('template_url')?>/img/fon.jpg')">
-    body
+<div class="main" style="background-image: url('<?php bloginfo('template_url')?>/img/fon.jpg')">
+    <img id="logo" src="<?php bloginfo('template_url')?>/img/logo.png">
 </div>
 
 <!---Footer-->
